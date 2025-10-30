73c0b3c1509f994b2b79c2867fb8972c65015ab1
David <beeermejoDev@gmail.com>
2025-10-30 20:07:51 +0100

    pruebas JS

---
+++ Tema 2/apuntes/2.5 Bucles/bucles002.js
@@ -0,0 +1,17 @@
+
+
+        /*
+        while(condicion){
+            ejecicion
+        }
+
+        */
+
+let contador=1;
+
+    while (contador <= 10 ){
+        console.log(contador)
+        contador++
+    }
+
+
